97ab20a468d4d9171665106d3f6b07fbb343681e
Victor Murilo <victor2706.vm@gmail.com>
2022-04-20 15:33:53 -0300

    Cumpri o requisito 7

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
 window.onload = function () {
     document.querySelector('#preto').className = 'color selected'
 }
+
+let coiso = document.querySelector('#color-palette')
+coiso.addEventListener('click', function (event) {
+    if(event.target.className == 'color') {
+        let coiso2 = document.querySelectorAll('.selected')
+        coiso2[0].className = 'color'
+        event.target.className = 'color selected'
+    } else {
+    }
+})
