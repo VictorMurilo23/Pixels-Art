fbbe0acbf9a70a1e65266b55db06bff222b9aa0a
Victor Murilo <victor2706.vm@gmail.com>
2022-04-20 15:56:58 -0300

    Cumpri o requisito 8

--- script.js
+++ script.js
@@ -2,12 +2,22 @@ window.onload = function () {
     document.querySelector('#preto').className = 'color selected'
 }
 
-let coiso = document.querySelector('#color-palette')
-coiso.addEventListener('click', function (event) {
+let colorPalette = document.querySelector('#color-palette')
+colorPalette.addEventListener('click', function (event) {
     if(event.target.className == 'color') {
-        let coiso2 = document.querySelectorAll('.selected')
-        coiso2[0].className = 'color'
+        let colorSelected = document.querySelectorAll('.selected')
+        colorSelected[0].className = 'color'
         event.target.className = 'color selected'
+    } else {
+    }
+})
+
+let pixelBoard = document.querySelector('#pixel-board')
+pixelBoard.addEventListener('click', function (event) { // Eu consegui fazer essa função graças as dicas :D
+    if (event.target.className == 'pixel') {
+        let color = document.querySelector('.selected')
+        const cssObj = window.getComputedStyle(color)
+        event.target.style.backgroundColor = cssObj.getPropertyValue("background-color");
     } else {
     }
 })
